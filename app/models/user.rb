a741a04b680d247829d509d5c1e8dad145bf52dc
morishitami <kshion.with.cad@outlook.jp>
2021-12-09 15:30:21 +0900

    userモデルバリデーションの設定、新規登録画面の作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :code, format: {with: /\A\d{3}$|^\d{4}\z/, message: 'Please confirm "会員番号"'}
+    validates :name
+  end
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: ' Include both letters and numbers'}
+        
 end
+
+
