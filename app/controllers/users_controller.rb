1733f686776dd57a238f9d081dda55044f67d280
morishitami <kshion.with.cad@outlook.jp>
2021-12-11 01:18:17 +0900

    表作成方法の変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,6 @@ class UsersController < ApplicationController
     @scode = @code.to_s
     @xlsx = Roo::Excelx.new("test-data.xlsx")
     @sheet = @xlsx.sheet(@scode)
+    @last =@sheet.last_row - 4
   end
 end
