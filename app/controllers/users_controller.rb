91d105698e5070a41baaa61d279543e891022651
morishitami <kshion.with.cad@outlook.jp>
2021-12-10 17:26:35 +0900

    excelとの初期連携

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,7 @@ class UsersController < ApplicationController
   def show
     @code = current_user.code
     @name = current_user.name
+    @xlsx = Roo::Excelx.new("test-data.xlsx")
+    @sheet = @xlsx.sheet('101')
   end
 end
