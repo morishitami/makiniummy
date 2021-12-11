9e5014c055c6c87c1f9dd0ca6dcea3e723f7ab58
morishitami <kshion.with.cad@outlook.jp>
2021-12-12 02:48:12 +0900

    マイページの見た目調整

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,8 +1,10 @@
 class ActorsController < ApplicationController
   def index
-    @code = current_user.code
-    @scode = @code.to_s
-    @xlsx = Roo::Excelx.new("test-data.xlsx")
-    @sheet = @xlsx.sheet(@scode)
+    if user_signed_in?
+      @code = current_user.code
+      @scode = @code.to_s
+      @xlsx = Roo::Excelx.new("test-data.xlsx")
+      @sheet = @xlsx.sheet(@scode)
+    end
   end
 end
