e425a7e64aa1cecf2083e0544488ad8aa171cda2
morishitami <kshion.with.cad@outlook.jp>
2021-12-12 00:43:45 +0900

    stage_nameの反映、indexへ成績反映

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,4 +1,8 @@
 class ActorsController < ApplicationController
   def index
+    @code = current_user.code
+    @scode = @code.to_s
+    @xlsx = Roo::Excelx.new("test-data.xlsx")
+    @sheet = @xlsx.sheet(@scode)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
+  
   def show
     @code = current_user.code
-    @name = current_user.stage_name
+    @stage_name = current_user.stage_name
+    @name = current_user.name
     @scode = @code.to_s
     @xlsx = Roo::Excelx.new("test-data.xlsx")
     @sheet = @xlsx.sheet(@scode)
     @last =@sheet.last_row - 4
   end
 end
+
