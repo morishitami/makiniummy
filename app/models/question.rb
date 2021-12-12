6d8f861e161034281857ed083a10dde6157e10f3
morishitami <kshion.with.cad@outlook.jp>
2021-12-12 21:52:00 +0900

    バリデーションの修正

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,31 @@
 class Question < ApplicationRecord
+  with_options presence: true do
+    validates :q1
+    validates :q2
+    validates :q3
+    validates :q4
+    validates :q5
+    validates :q6
+    validates :q7
+    validates :q8
+    validates :q9
+    validates :q10
+    validates :q11
+    validates :q12
+    validates :q13
+    validates :q14
+    validates :q15
+    validates :q16
+    validates :q17
+    validates :q18
+    validates :q19
+    validates :q20
+    validates :q21
+    validates :q22
+    validates :q23
+    validates :q24
+    validates :q25
+  end
+
   belongs_to :user
 end
