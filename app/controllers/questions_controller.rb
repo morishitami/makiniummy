0366076a7767f851a05b0a4824dea75f34c1dfde
morishitami <kshion.with.cad@outlook.jp>
2021-12-13 17:55:37 +0900

    createアクションの作成

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,9 @@ class QuestionsController < ApplicationController
   def show    
     @questions = Question.new
   end
+
+  def create
+    @question = Question.create(question_params)
+    redirect_to question_path(@question.actor)
+  end
 end
