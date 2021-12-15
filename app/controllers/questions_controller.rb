280728ac66a6b387f63c0fe8f9f9929d6b94f24a
morishitami <kshion.with.cad@outlook.jp>
2021-12-15 11:50:44 +0900

    editビューファイルの削除

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,7 +5,7 @@ class QuestionsController < ApplicationController
 
   def create
     @question = Question.create!(question_params)
-    redirect_to edit_user_question_path(@question.user, @question)
+    redirect_to root_path
   end
 
   def edit
@@ -15,7 +15,7 @@ class QuestionsController < ApplicationController
   def update
     @question = Question.find(params[:id])
     @question.update(question_params)
-    redirect_to edit_user_question_path(@question.user, @question)
+    redirect_to root_path
   end
 
 
