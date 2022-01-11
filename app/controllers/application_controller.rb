cde34f5ac6cffb919ce3cd1f2b305846c1939df1
morishitami <kshion.with.cad@outlook.jp>
2022-01-11 14:58:55 +0900

    authenticate_user!の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, except: :index
 
   private
 
@@ -12,6 +13,5 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :stage_name, :email])
-
   end
 end
